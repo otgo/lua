84e60b2514cecbdf89607b9d3e62489e889d20e7
Harry <otgo@outlook.es>
2019-05-17 10:04:47 -0500

    wfrs 0.9

--- lua/5.2/wfrs.c
+++ lua/5.2/wfrs.c
@@ -5,10 +5,9 @@
 #include <dirent.h>
 int atoi(const char *string);
 size_t null_data(void *buffer, size_t size, size_t nmemb, void *userp);
-char* concat(const char *string_1, const char *string_2);
 int file_exists(const char * filename);
 int dir_exists(char *dirname);
-int startsWith(const char *pre, const char *str);
+int IsComment(const char *__line__);
 int main(int argc, char **argv) {
     CURL *curl;
     CURLcode res;
@@ -21,51 +20,66 @@ int main(int argc, char **argv) {
             printf("wfrs: usage: wfrs lua_script_name (optional)file_for_redis_config\n");
             return 0;
         } else {
-            char *__lua_script__ = concat("/home/telegram-bot/", argv[1]);
-            __lua_script__ = concat(__lua_script__, ".lua");
+            char *__lua_script__ = malloc(23+strlen(argv[1]));
+            sprintf(__lua_script__, "/home/telegram-bot/%s.lua", argv[1]);
             if (!file_exists(__lua_script__)) {
-                char *__ffd__ = concat("wfrs: cannot open ", __lua_script__);
-                __ffd__ = concat(__ffd__, " No such file");
-                fprintf(stderr, "%s\n", __ffd__);
-                free(__ffd__); free(__lua_script__);
+                fprintf(stderr, "wfrs: cannot open %s: No such file\n", __lua_script__);
+                free(__lua_script__);
                 return 1;
             }
             free(__lua_script__);
         }
         if (argv[2] != NULL) {
-            if (!file_exists(argv[2])) {
-                char *__ffd__ = concat("wfrs: cannot open ", argv[2]);
-                __ffd__ = concat(__ffd__, ": No such file");
-                fprintf(stderr, "%s\n", __ffd__);
-                free(__ffd__);
+            FILE * fp;
+            fp = fopen(argv[2], "r");
+            if (!fp) {
+                fprintf(stderr, "wfrs: cannot open %s: No such file\n", argv[2]);
                 return 1;
             }
-            FILE * fp;
-            size_t len;
+            size_t buff = 0;
             ssize_t read;
             char *line, *tmp_val, *tmp_var, *var_find;
-            int position, len_line;
-            tmp_val = malloc(sizeof(char));
-            tmp_var = malloc(sizeof(char));
-            line = malloc(sizeof(char));
-            len = 0;
-            fp = fopen(argv[2], "r");
-            while ((read = getline(&line, &len, fp)) != -1 && strlen(line) > 1) {
+            int position, len, tmp_val_len, line_num;
+            tmp_val = malloc(sizeof(char*));
+            tmp_var = malloc(sizeof(char*));
+            line = malloc(sizeof(char*)*32);
+            line_num = 1;
+            while ((read = getline(&line, &buff, fp)) != -1) {
                 strtok(line, "\n");
-                if (startsWith("#", line) == 0) {
-                    len_line = strlen(line);
-                    var_find = strchr(line, '"');
-                    if (var_find == NULL) var_find = strchr(line, '\'');
-                    if (var_find == NULL) continue;
-                    position = (var_find-line);
+                len = strlen(line);
+                if (IsComment(line) == 0) {
+                    var_find = strchr(line, '=');
+                    position = var_find-line;
                     tmp_var = (char *)realloc(tmp_var, position);
-                    strncpy(tmp_var, line, position-1);
-                    tmp_val = (char *)realloc(tmp_val, len_line-position);
-                    strcpy(tmp_val, &line[position]+1);
-                    tmp_var[position-1] = '\0';
-                    tmp_val[strlen(tmp_val)-1] = '\0';
+                    strncpy(tmp_var, line, position);
+                    tmp_var[position] = '\0';
+                    if (line[position+1] == '"' || line[position+1] == '\'') {
+                        tmp_val = (char *)realloc(tmp_val, len-position);
+                        strcpy(tmp_val, &line[position+2]);
+                        int last_poc;
+                        if (strrchr(tmp_val, '"')) last_poc = strrchr(tmp_val, '"')-tmp_val;
+                        else if (strrchr(tmp_val, '\'')) last_poc = strrchr(tmp_val, '\'')-tmp_val;
+                        else {
+                            // error unfinished string ex. HELLO="hello
+                            fprintf(stderr, "wfrs: %s:%i: warning: unfinished string near '%s'\n", argv[2], line_num, tmp_val);
+                            continue;
+                        }
+                        tmp_val[last_poc] = '\0'; // delete last character " or '
+                        // add var in line ex. HELLO="hello" HELLO='hello'
+                    } else if (line[len-1] == '"' || line[len-1] == '\'') {
+                        // error unfinished string ex. HELLO=hello"
+                        fprintf(stderr, "wfrs: %s:%i: warning: unfinished string near '%s'\n", argv[2], line_num, tmp_val);
+                        continue;
+                    } else {
+                        // add var in line ex. HELLO=hello
+                        tmp_val = (char *)realloc(tmp_val, len-position);
+                        strcpy(tmp_val, &line[position+1]);
+                        strtok(tmp_val, " ");
+                    }
+                    int last_poc = strrchr(line, '"')-line;
                     setenv(tmp_var, tmp_val, 1);
                 }
+                line_num++;
             }
             fclose(fp);
             free(line); free(tmp_var); free(tmp_val);
@@ -73,12 +87,12 @@ int main(int argc, char **argv) {
     }
     char *redis_host = getenv("REDIS_HOST");
     char *redis_port_str = getenv("REDIS_PORT");
-    printf("\033[1mRunning Redis...\033[0m\n");
+    printf("wfrs: loading redis-server...\n");
     if (redis_host == NULL) {
-        fprintf(stderr, "\033[1;31mError while running redis\033[0m, host not found.\n\tRun: export REDIS_HOST='redis_host'\n\tor: wfrs script_to_init.lua file_for_redis_config.env\n");
+        fprintf(stderr, "wfrs: error loading redis-server, environment variable REDIS_HOST not found.\n\trun: export REDIS_HOST='redis_host'\n\tor: wfrs script_to_init.lua file_for_redis_config.env\n");
         return 1;
     } else if (redis_port_str == NULL) {
-        fprintf(stderr, "\033[1;31mError while running redis\033[0m, port not found.\n\tRun: export REDIS_PORT='redis_port'\n\tor: wfrs script_to_init.lua file_for_redis_config.env\n");
+        fprintf(stderr, "wfrs: error loading redis-server, environment variable REDIS_PORT not found.\n\trun: export REDIS_PORT='redis_port'\n\tor: wfrs script_to_init.lua file_for_redis_config.env\n");
         return 1;
     }
     curl = curl_easy_init();
@@ -89,15 +103,17 @@ int main(int argc, char **argv) {
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, null_data);
         res = curl_easy_perform(curl);
         if(res != CURLE_OK) {
-            fprintf(stderr, "\033[1;31mError while running redis\033[0m, response not found, check your port/host redis.\n");
+            fprintf(stderr, "wfrs: error while running redis-server (%s:%d), response not found, check your port/host redis.\n", redis_host, redis_port);
             return_code = 1;
         } else {
-            printf("\033[1;32mScript running.\033[0m\n\t\033[1mREDIS_HOST\033[0m %s\n\t\033[1mREDIS_PORT\033[0m %i\n", redis_host, redis_port);
-            printf("Running bot.lua...");
+            printf("redis-server (%s:%d) running.\n", redis_host, redis_port);
+            printf(" bot.lua...");
             curl_easy_cleanup(curl);
-            char *order_bash = concat("cd /home/telegram-bot ; lua '", argv[1]);
-            order_bash = concat(order_bash, ".lua'");
-            system(order_bash);
+
+            char *__order_bash__ = malloc(32+strlen(argv[1]));
+            sprintf(__order_bash__, "cd /home/telegram-bot ; lua %s.lua", argv[1]);
+            system(__order_bash__);
+            free(__order_bash__);
             return 0;
         }
         curl_easy_cleanup(curl);
@@ -106,14 +122,6 @@ int main(int argc, char **argv) {
 }
 size_t null_data(void *buffer, size_t size, size_t nmemb, void *userp) {
     return size * nmemb;
-}
-char* concat(const char *string_1, const char *string_2) {
-    const size_t length_1 = strlen(string_1);
-    const size_t length_2 = strlen(string_2);
-    char *result = malloc(length_1 + length_2 + 1);
-    memcpy(result, string_1, length_1);
-    memcpy(result + length_1, string_2, length_2 + 1);
-    return result;
 }
 int file_exists(const char * filename) {
     FILE *file;
@@ -132,7 +140,6 @@ int dir_exists(char *dirname) {
     }
     return 0;
 }
-int startsWith(const char *pre, const char *str) {
-    size_t lenpre = strlen(pre), lenstr = strlen(str);
-    return lenstr < lenpre ? 0 : strncmp(pre, str, lenpre) == 0;
+int IsComment(const char *__line__) {
+    return strncmp("#", __line__, 1) == 0;
 }
